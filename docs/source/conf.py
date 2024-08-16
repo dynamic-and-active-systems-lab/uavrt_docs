df9a9339ecaf66eda92ec07c50eb08df98a5fa1a
mwshafer <michael.w.shafer@gmail.com>
2024-08-16 09:06:59 -0700

    changed config file to use uav-rt project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'UAV-RT'
+copyright = '2024, Shafer'
+author = 'Shafer'
 
 release = '0.1'
 version = '0.1.0'
